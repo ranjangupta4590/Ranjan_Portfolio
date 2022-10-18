0ff2af5564ef4239d4924d2ff5f301f8f0e51545
ranjan_4590 <ranjangupta35558@gmail.com>
2022-10-18 20:14:04 +0530

    about update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,34 +35,9 @@ const About = () => {
               requirements of industry.
               My strength is that,I can easily shape myself according to the industrial environment.
             </p>
-            {/* <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>   */}
+            
           </div>
         </div>
-        {/* <div className="pt-8">
-          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-            <div className="sm:text-right pb-8 pl-4">
-              <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-                StartUp
-              </p>
-            </div>
-            <div></div>
-          </div>
-          <div className="flex flex-col justify-center items-center w-full h-full   text-gray-300">
-            <p className="">
-              Being a co-founder of Quickondoor my responsibility was to look
-              over the procedure of delivering orders and to connect people with
-              local vendors. We used to deliver grocery and bakery items.
-              Customer used to connect with us through our website and whatsApp.
-              In 2 months we have delivered more than 400+ orders but due to
-              lack of fund and unable to generate profit in small town we have
-              to close it.
-            </p>
-          </div>
-        </div> */}
       </div>
     </div>
   );
